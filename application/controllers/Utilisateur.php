e4382ff1f30c8bfbb6e72fdaf11cdd6f34b51ecf
elsa.m <manangelsa07@gmail.com>
2018-11-30 15:08:24 +0100

    update

--- application/controllers/Utilisateur.php
+++ application/controllers/Utilisateur.php
@@ -5,9 +5,36 @@ class Utilisateur extends CI_Controller
 {
     public function form_inscription()
     {
-        #$this->load->view('mokapi_home.php');
+        $this->load->view('form_inscription.php');
     }
     public function form_authentification(){
-        
+        $this->load->view('form_authentification.php');
+    }
+    public function nouvel_utilisateur(){
+        $nomcomplet = $this->input->post('nomcomplet');
+        $login = $this->input->post('login');
+        $mdp = $this->input->post('mdp');
+        $mdp_conf = $this->input->post('mdp_conf');
+        $email = $this->input->post('email');
+        if($mdp == $mdp_conf){
+            $this->load->model('UtilisateurModel');
+            $info= array(
+                'id'=> NULL,
+                'nomcomplet'=> $nomcomplet,
+                'login'=> $login,
+                'mdp'=> $mdp,
+                'email'=> $email,
+                'etat'=> FALSE
+
+            );
+            $this->UtilisateurModel->creer_utilisateur($info);
+            $lien = site_url();
+            echo "inscription reussie <br> <a href='$lien'>acceuil</a>";
+
+        }
+        else{
+            echo "Mot de passe non identique";
+            $this->load->view('form_inscription.php');
+        }
     }
 }
