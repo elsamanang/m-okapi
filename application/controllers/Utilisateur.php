6b4b3552b1ece635c4ac04000381e5e96796d363
elsa.m <manangelsa07@gmail.com>
2018-11-30 13:45:40 +0100

    new

---
+++ application/controllers/Utilisateur.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Utilisateur extends CI_Controller
+{
+    public function form_inscription()
+    {
+        #$this->load->view('mokapi_home.php');
+    }
+    public function form_authentification(){
+        
+    }
+}
