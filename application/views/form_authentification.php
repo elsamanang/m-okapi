d42b8e4c31aefb10e9f5f5f994a80c2624e3877b
elsa.m <manangelsa07@gmail.com>
2018-12-04 09:25:50 +0100

    update

--- application/views/form_authentification.php
+++ application/views/form_authentification.php
@@ -1,6 +1,9 @@
 <h1>Authentification</h1>
-<form method="post" action="<?php site_url('utilisateur/connexion')?>">
-    login : <input type="text" name="login"><br>
-    mot de passe : <input type="password" name="mdp"><br>
-    <input type="submit" value="connexion"><br>
+<form method="post" action="<?php echo site_url('utilisateur/connexion')?>">
+    login : <input type="text" name="login"><br><br>
+    mot de passe : <input type="password" name="mdp"><br><br>
+    <input type="submit" value="connexion"><br><br>
+    <p style="color: red;"><?php
+        echo $this->session->erreur;
+    ?></p>
 </form>
