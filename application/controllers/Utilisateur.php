f3d8e1ce68675261d1030930b62d0c5bc4b3f11e
elsa.m <manangelsa07@gmail.com>
2018-12-04 08:21:11 +0100

    update

--- application/controllers/Utilisateur.php
+++ application/controllers/Utilisateur.php
@@ -3,6 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Utilisateur extends CI_Controller
 {
+    public function __construct()
+    {
+        $this->load->model('UtilisateurModel');
+    }
     public function form_inscription()
     {
         $this->load->view('form_inscription.php');
@@ -17,24 +21,49 @@ class Utilisateur extends CI_Controller
         $mdp_conf = $this->input->post('mdp_conf');
         $email = $this->input->post('email');
         if($mdp == $mdp_conf){
-            $this->load->model('UtilisateurModel');
-            $info= array(
-                'id'=> NULL,
-                'nomcomplet'=> $nomcomplet,
-                'login'=> $login,
-                'mdp'=> $mdp,
-                'email'=> $email,
-                'etat'=> FALSE
+            if(strlen($login) == 8){
+                $this->load->library('form_validation');
+                $this->form_validation->set_rules('email', '"le mail est invalide"', 'trim|required|min_length[5]|max_length[20]|alpha dash|encode_php_tags|xss_clean');
+                if(){
+                    $info= array(
+                        'id'=> NULL,
+                        'nomcomplet'=> $nomcomplet,
+                        'login'=> $login,
+                        'mdp'=> $mdp,
+                        'email'=> $email,
+                        'etat'=> FALSE
 
-            );
-            $this->UtilisateurModel->creer_utilisateur($info);
-            $lien = site_url();
-            echo "inscription reussie <br> <a href='$lien'>acceuil</a>";
+                    );
+                    $this->UtilisateurModel->creer_utilisateur($info);
+                    $lien = site_url();
+                    echo "inscription reussie <br> <a href='$lien'>acceuil</a>";
+                }
+                
 
+            }
+            else{
+                echo "le mot de passe doit avoir au moins 8 caracteres";
+                $this->load->view('form_inscription.php');
+            }
         }
         else{
             echo "Mot de passe non identique";
             $this->load->view('form_inscription.php');
+        }
+    }
+    public function connexion(){
+        $login = $this->input->post('login');
+        $mdp = $this->input->post('mdp');
+        $d = array(
+            'login' => $login,
+            'mdp' => $mdp
+        );
+        $r = $this->utilisateurModel->login($d);
+        if(count($r) > 0){
+            echo "coool";
+        }
+        else{
+            echo "bad";
         }
     }
 }
