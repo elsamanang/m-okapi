1354a35c9662e300b1254731099ba4a023b19aac
elsa.m <manangelsa07@gmail.com>
2018-12-04 09:25:39 +0100

    update

--- application/controllers/Utilisateur.php
+++ application/controllers/Utilisateur.php
@@ -5,6 +5,7 @@ class Utilisateur extends CI_Controller
 {
     public function __construct()
     {
+        parent::__construct();
         $this->load->model('UtilisateurModel');
     }
     public function form_inscription()
@@ -24,7 +25,7 @@ class Utilisateur extends CI_Controller
             if(strlen($login) == 8){
                 $this->load->library('form_validation');
                 $this->form_validation->set_rules('email', '"le mail est invalide"', 'trim|required|min_length[5]|max_length[20]|alpha dash|encode_php_tags|xss_clean');
-                if(){
+                if(a){
                     $info= array(
                         'id'=> NULL,
                         'nomcomplet'=> $nomcomplet,
@@ -58,12 +59,38 @@ class Utilisateur extends CI_Controller
             'login' => $login,
             'mdp' => $mdp
         );
-        $r = $this->utilisateurModel->login($d);
+        
+        $r = $this->UtilisateurModel->login($d);
         if(count($r) > 0){
-            echo "coool";
+            $user = $r[0];
+            $d = array(
+                'id' => $user->id,
+                'nomcomplet' => $user->nomcomplet,
+                'is_connected' => true
+            );
+            $this->session->set_userdata($d);
+            redirect('utilisateur/acceuil');
+            
         }
         else{
-            echo "bad";
+            $error = array(
+                'erreur'=> 'login ou mot de passe incorrect'
+            );
+            $this->session->set_flashdata($error);
+            $this->load->view('form_authentification');
         }
+    }
+    public function acceuil(){
+        if($this->session->is_connected){
+            $this->load->view('utilisateur/acceuil');
+        }
+        else{
+            redirect();
+            
+        }
+    }
+    public function logout(){
+        session_destroy();
+        redirect();
     }
 }
