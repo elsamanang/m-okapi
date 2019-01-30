0beb31d4be8c172d5ea8f262ccc1222ff13e68b6
elsa.m <manangelsa07@gmail.com>
2019-01-30 15:52:25 +0100

    new

---
+++ application/views/header.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!doctype html> 
+<html> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="description" content="M-Okapi">
+    <title>M-Okapi</title>
+    <link href="<?php echo base_url("assets/css/bootstrap.min.css");?>" rel="stylesheet">    <link href="<?php echo base_url("assets/font-awesome/css/font-awesome.min.css"); ;?>" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Arvo:400,700' rel='stylesheet' type='text/css'>
+    <link href="<?php echo base_url("assets/css/general.css");?>" rel="stylesheet">
+    
+     <!-- Owl-Carousel -->
+    <link href="<?php echo base_url("assets/css/custom.css");?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/owl.carousel.css");?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/owl.theme.css");?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/style.css");?>" rel="stylesheet">
+    <link href="<?php echo base_url("assets/css/animate.css");?>" rel="stylesheet">
+    
+    <!-- Magnific Popup core CSS file -->
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/magnific-popup.css");?>"> 
+    
+    <script src="<?php echo base_url("assets/js/modernizr-2.8.3.min.js");?>"></script>  <!-- Modernizr /-->
+    
+
+</head>
