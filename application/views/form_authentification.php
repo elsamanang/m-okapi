69b0e04722979e99b317c8003ee8c5568498fae9
elsa.m <manangelsa07@gmail.com>
2019-01-30 15:47:43 +0100

    new

--- application/views/form_authentification.php
+++
@@ -1,9 +0,0 @@
-<h1>Authentification</h1>
-<form method="post" action="<?php echo site_url('utilisateur/connexion')?>">
-    login : <input type="text" name="login"><br><br>
-    mot de passe : <input type="password" name="mdp"><br><br>
-    <input type="submit" value="connexion"><br><br>
-    <p style="color: red;"><?php
-        echo $this->session->erreur;
-    ?></p>
-</form>
