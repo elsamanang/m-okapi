5237ccc1ee121f6c85314452bef32eb62345aa0b
elsa.m <manangelsa07@gmail.com>
2019-01-30 15:52:17 +0100

    new

---
+++ application/views/footer.php
@@ -0,0 +1,28 @@
+<script src="<?php echo base_url("assets/js/jquery-1.10.2.js");?>"></script>
+    <script src="<?php echo base_url("assets/js/bootstrap.js");?>"></script>
+    <script src="<?php echo base_url("assets/js/owl.carousel.js");?>"></script>
+    <script src="<?php echo base_url("assets/js/script.js");?>"></script>
+    <!-- StikyMenu -->
+    <script src="<?php echo base_url("assets/js/stickUp.min.js");?>"></script>
+    <script type="text/javascript">
+      jQuery(function($) {
+        $(document).ready( function() {
+          $('.navbar-default').stickUp();
+          
+        });
+      });
+    
+    </script>
+    <!-- Smoothscroll -->
+    <script type="text/javascript" src="<?php echo base_url("assets/js/jquery.corner.js");?>"></script> 
+    <script src="<?php echo base_url("assets/js/wow.min.js");?>"></script>
+    <script>
+     new WOW().init();
+    </script>
+    <script src="<?php echo base_url("assets/js/classie.js");?>"></script>
+    <script src="<?php echo base_url("assets/js/uiMorphingButton_inflow.js");?>"></script>
+    <!-- Magnific Popup core JS file -->
+    <script src="<?php echo base_url("assets/js/jquery.magnific-popup.js");?>"></script> 
+</body>
+
+</html>
