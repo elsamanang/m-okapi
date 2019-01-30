3baf8b13a3b0d37adf9c130828b3572be87cfc00
elsa.m <manangelsa07@gmail.com>
2019-01-30 16:16:51 +0100

    update

--- application/controllers/MOkapi.php
+++ application/controllers/MOkapi.php
@@ -8,6 +8,8 @@ class MOkapi extends CI_Controller
         if($this->session->is_connected){
             $this->load->view('utilisateur/acceuil');
         }
+        $this->load->view('header.php');
         $this->load->view('mokapi_home.php');
+        $this->load->view('footer.php');
     }
 }
