62058d69d519cafae44bcd1bf3f72a6eac80cc0a
elsa.m <manangelsa07@gmail.com>
2019-01-30 15:46:37 +0100

    update

--- application/controllers/Utilisateur.php
+++ application/controllers/Utilisateur.php
@@ -22,11 +22,7 @@ class Utilisateur extends CI_Controller
         $mdp_conf = $this->input->post('mdp_conf');
         $email = $this->input->post('email');
         if($mdp == $mdp_conf){
-            if(strlen($login) == 8){
-                $this->load->library('form_validation');
-                $this->form_validation->set_rules('email', '"le mail est invalide"', 'trim|required|min_length[5]|max_length[20]|alpha dash|encode_php_tags|xss_clean');
-                if(a){
-                    $info= array(
+            $info= array(
                         'id'=> NULL,
                         'nomcomplet'=> $nomcomplet,
                         'login'=> $login,
@@ -35,17 +31,9 @@ class Utilisateur extends CI_Controller
                         'etat'=> FALSE
 
                     );
-                    $this->UtilisateurModel->creer_utilisateur($info);
-                    $lien = site_url();
-                    echo "inscription reussie <br> <a href='$lien'>acceuil</a>";
-                }
-                
-
-            }
-            else{
-                echo "le mot de passe doit avoir au moins 8 caracteres";
-                $this->load->view('form_inscription.php');
-            }
+            $this->UtilisateurModel->creer_utilisateur($info);
+            $lien = site_url();
+            echo "inscription reussie <br> <a href='$lien'>acceuil</a>";
         }
         else{
             echo "Mot de passe non identique";
