c865459e1413851c1773ec1746187834ba428a36
elsa.m <manangelsa07@gmail.com>
2019-01-31 10:32:41 +0100

    update

--- application/controllers/MOkapi.php
+++ application/controllers/MOkapi.php
@@ -12,4 +12,39 @@ class MOkapi extends CI_Controller
         $this->load->view('mokapi_home.php');
         $this->load->view('footer.php');
     }
+
+    //====================================================================================
+
+    public function add_entree() {
+        $this->form_validation->set_rules('nom','nom','required');
+        $this->form_validation->set_rules('montant','montant','required');
+        if($this->form_validation->run()){
+            $nom = $this->input->post('nom');
+            $montant = $this->input->post('montant');
+            $user = $this->session->id;
+            $date_ent = date('Y-m-d');
+            #mettre dans le model
+        }
+    }
+    public function add_catgorie_sortie() {
+        $this->form_validation->set_rules('nom','nom','required');
+        if($this->form_validation->run()){
+            $nom = $this->input->post('nom');
+            $user = $this->session->id;
+            #mettre dans le model
+        }
+    }
+    public function add_exercice_budgetaire() {
+        $this->form_validation->set_rules('datedebut','datedebut','required');
+        $this->form_validation->set_rules('datefin','datefin','required');
+        $this->form_validation->set_rules('budgetinitial','budgetinitial','required');
+        if($this->form_validation->run()){
+            $datedebut = $this->input->post('datedebut');
+            $datefin = $this->input->post('datefin');
+            $budgetinitial = $this->input->post('budgetinitial');
+            $user = $this->session->id;
+            $date_creation = date('Y-m-d');
+            #mettre dans le model
+        }
+    }
 }
